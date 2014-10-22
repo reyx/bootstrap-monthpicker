5c4135efdc045b00b1ec41110474529a22b8d8a7
reyx <regis.sabino@gmail.com>
2014-10-21 22:20:19 -0200

    build-8

--- bootstrap-monthpicker.js
+++ bootstrap-monthpicker.js
@@ -1,54 +1,56 @@
 'use strict';
-angular.module('ui.bootstrap-month-picker', []).directive('monthPicker', function ($http, $modal, $timeout) {
-    return {
-        restrict: 'A',
-        require: '^ngModel',
-        templateUrl: 'bootstrap-monthpicker/bootstrap-monthpicker.html',
-        link: function ($scope, $element, $attrs, ngModel) {
-            $scope.periods = [];
+angular.module('ui.bootstrap-month-picker', []).directive('monthPicker', ['$parse',
+    function ($parse) {
+        return {
+            restrict: 'A',
+            require: '^ngModel',
+            templateUrl: 'bootstrap-monthpicker/bootstrap-monthpicker.html',
+            link: function ($scope, $element, $attrs, ngModel) {
+                $scope.periods = [];
 
-            $scope.setPeriod = function (period) {
-                $scope.periods.length = 0;
-                $scope.brand = $attrs.monthPickerBrand;
+                $scope.setPeriod = function (period) {
+                    $scope.periods.length = 0;
+                    $scope.brand = $attrs.monthPickerBrand;
 
-                var start = window.moment(period).subtract(3, 'months').toDate();
+                    var start = window.moment(period).subtract(3, 'months').toDate();
 
-                for (var i = 0; i < 7; i++) {
-                    var idx = i;
+                    for (var i = 0; i < 7; i++) {
+                        var idx = i;
 
-                    if (i === 0) {
-                        idx = 2;
+                        if (i === 0) {
+                            idx = 2;
+                        }
+                        else if (i === 6) {
+                            idx = 4;
+                        }
+
+                        $scope.periods.push({
+                            active: i === 3,
+                            date: window.moment(start).add(idx, 'months').toDate()
+                        });
                     }
-                    else if (i === 6) {
-                        idx = 4;
+
+                    ngModel.$setViewValue(period);
+
+                    if ($attrs.onMonthChange) {
+                        var change = $attrs.onMonthChange.substring(0, $attrs.onMonthChange.indexOf('('));
+                        var _scope = $scope;
+                        while (typeof (_scope[change]) === 'undefined') {
+                            _scope = _scope.$parent;
+                        }
+                        $parse($attrs.onMonthChange)(_scope, { period: period });
                     }
+                };
 
-                    $scope.periods.push({
-                        active: i === 3,
-                        date: window.moment(start).add(idx, 'months').toDate()
-                    });
-                }
+                $scope.goToCurrentMonth = function () {
+                    $scope.setPeriod(new Date());
+                };
 
-                ngModel.$setViewValue(period);
-
-                if ($attrs.onMonthChange) {
-                    var change = $attrs.onMonthChange.substring(0, $attrs.onMonthChange.indexOf('('));
-                    var _scope = $scope;
-                    while (typeof (_scope[change]) === 'undefined') {
-                        _scope = _scope.$parent;
-                    }
-                    $parse($attrs.onMonthChange)(_scope, { period: period });
-                }
-            };
-
-            $scope.goToCurrentMonth = function () {
-                $scope.setPeriod(new Date());
-            };
-
-            $scope.goToCurrentMonth();
-        }
-    };
-});
+                $scope.goToCurrentMonth();
+            }
+        };
+    }
+]);
 
 angular.module('ui.bootstrap-month-picker').run(['$templateCache', function ($templateCache) {
     $templateCache.put('bootstrap-monthpicker/bootstrap-monthpicker.html', '<button data-ng-click="goToCurrentMonth()" class="btn btn-default">  <i class="fa fa-calendar-o fa-only"></i></button><button data-ng-repeat="period in periods" data-ng-class="period.active ? \'btn-\' + brand : \'\'" data-ng-click="setPeriod(period.date)" class="btn btn-default half-left">  <i data-ng-if="$first" class="fa fa-chevron-left fa-only"></i>  <i data-ng-if="$last" class="fa fa-chevron-right fa-only"></i>  <span data-ng-if="!$first &amp;&amp; !$last" data-ng-bind="period.date | date: \'MMM\'" class="uppercase"></span>  <span data-ng-if="!$first &amp;&amp; !$last" data-ng-bind="period.date | date: \'yyyy\'"></span></button>');
